5e87dd6b699cd098dfa7a742e612453eefe16890
Jaime <javial@floridauniversitaria.es>
2023-10-03 16:41:00 +0200

    subida inicial

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
+    <SafeAreaView style={styles.wrapper}> 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your POLLA!</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    
+    width: '50%',
+  },
+  wrapper: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'yellow',
   },
 });
